3e96af9f3087f6e3fd783f3563744d6e16936990
LucasGuardia4 <lbarozzi@guardiaschool.fr>
2024-12-19 10:44:21 +0100

    add db connection pour username

--- index.php
+++ index.php
@@ -1,10 +1,26 @@
 <?php require_once 'head.php'; ?>
 <?php require_once 'nav_bar.php'; ?>
 
+<?php
+require 'functions/db_operations.php';
+$pdo = connection_db();
+
+// Traite les erreurs potentielles lors de l'ouverture de la bdd
+if ($pdo[0] === false) {
+    die($pdo[1]);
+} else {
+    $pdo = $pdo[1];
+}
+$info = recover_user_info($pdo, 1);
+$firstname = $info['firstname'];
+$lastname = $info['lastname'];
+
+?>
+
 <section id="accueil" class="hero-section">
     <div class="tech-decoration top-left"></div>
     <div class="hero-content">
-        <h1>Dark Vador</h1>
+        <h1><?php echo $firstname . ' ' . $lastname; ?></h1>
         <h2 class="tech-title">Architecte des Défenses Impériales</h2>
         <p class="tagline">"La sécurité n'est pas une option. C'est une question de survie."</p>
         <div class="hero-cta">
